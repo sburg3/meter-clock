82174a9abcb05a6e78082a8e3a3ac92e88b6a7e5
sburg3 <sburg3@users.noreply.github.com>
2016-05-23 15:29:36 -0400

    Initial commit

---
+++ meter-clock/main.c
@@ -0,0 +1,18 @@
+/*
+ * meter-clock.c
+ *
+ * Created: 5/23/2016 3:25:41 PM
+ * Author : Steven
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
